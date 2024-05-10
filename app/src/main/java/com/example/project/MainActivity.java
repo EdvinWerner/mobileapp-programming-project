54abacdade2b8849f5d04864314e7f3fddc17d2b
Edde2508 <edvin.werner@live.se>
2024-05-10 12:45:32 +0200

    Gjorde alla stegen för att visa datan från Jsonfilen

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,58 @@
+
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.project.RecyclerViewAdapter;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a23edvwe";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        new JsonTask(this).execute(JSON_URL);
     }
 
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Motorbana>>() {}.getType();
+        List<Motorbana> ListOfMotorbanas = gson.fromJson(json, type);
+        ArrayList<Motorbana> items = new ArrayList<>();
+
+        for (Motorbana Motorbana : ListOfMotorbanas) {
+            Log.d("Motorbana", Motorbana.toString());
+            items.add(new Motorbana(Motorbana.getName()));
+        }
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Motorbana item) {
+                Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RecyclerView recycler_view = findViewById(R.id.recycler_view);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        recycler_view.setAdapter(adapter);
+    }
 }
--- app/src/main/java/com/example/project/Motorbana.java
+++ app/src/main/java/com/example/project/Motorbana.java
@@ -1,4 +1,27 @@
 package com.example.project;
 
 public class Motorbana {
+
+    //Member variables and state
+    private String name;
+
+    //Constructor
+    public Motorbana(String name) {
+        this.name = name;
+    }
+
+    //Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Interface
+    @Override
+    public String toString(){
+        return name;
+    }
 }
