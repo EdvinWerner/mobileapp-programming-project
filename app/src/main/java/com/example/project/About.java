b1ddc7a91286743751572bab14eb718365eab638
Edde2508 <edvin.werner@live.se>
2024-05-10 13:50:55 +0200

    Moved asset folder to correct place and created a webview for the about.html page in About.java

--- app/src/main/java/com/example/project/About.java
+++ app/src/main/java/com/example/project/About.java
@@ -1,12 +1,33 @@
 package com.example.project;
 
+import static com.example.project.R.*;
+
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 public class About extends AppCompatActivity {
+
+    WebView myWebView;
+
+    public void showInternalWebPage() {
+        myWebView.loadUrl("file:///android_asset/about.html");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        myWebView = findViewById(R.id.My_WebView);
+        myWebView.setWebViewClient(new WebViewClient()); // Do not open in Chrome!
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        showInternalWebPage();
     }
 }
